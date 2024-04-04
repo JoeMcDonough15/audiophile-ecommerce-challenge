8c3dc5b516f4faa43a72697f40b6963b2e894a2f
Joe McDonough <joemcdonoughmusic@gmail.com>
2024-04-04 18:42:21 -0400

    Remove unused gallery-image-one class

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -18,34 +18,34 @@ const Gallery = ({ imageData }: Props): JSX.Element => {
       <div className="gallery-section-one col">
         <div className="gallery-image-one-container">
           <CustomImage
-            className={"gallery-image gallery-image-one image-mobile"}
+            className={"gallery-image image-mobile"}
             src={imageData.first.mobile}
             altText={imageData.first.imageAltText}
           />
           <CustomImage
-            className={"gallery-image gallery-image-one image-tablet"}
+            className={"gallery-image image-tablet"}
             src={imageData.first.tablet}
             altText={imageData.first.imageAltText}
           />
           <CustomImage
-            className={"gallery-image gallery-image-one image-desktop"}
+            className={"gallery-image image-desktop"}
             src={imageData.first.desktop}
             altText={imageData.first.imageAltText}
           />
         </div>
         <div className="gallery-image-two-container">
           <CustomImage
-            className={"gallery-image gallery-image-one image-mobile"}
+            className={"gallery-image image-mobile"}
             src={imageData.second.mobile}
             altText={imageData.second.imageAltText}
           />
           <CustomImage
-            className={"gallery-image gallery-image-one image-tablet"}
+            className={"gallery-image image-tablet"}
             src={imageData.second.tablet}
             altText={imageData.second.imageAltText}
           />
           <CustomImage
-            className={"gallery-image gallery-image-one image-desktop"}
+            className={"gallery-image image-desktop"}
             src={imageData.second.desktop}
             altText={imageData.second.imageAltText}
           />
@@ -54,17 +54,17 @@ const Gallery = ({ imageData }: Props): JSX.Element => {
       <div className="gallery-section-two">
         <div className="gallery-image-three-container">
           <CustomImage
-            className={"gallery-image gallery-image-one image-mobile"}
+            className={"gallery-image image-mobile"}
             src={imageData.third.mobile}
             altText={imageData.third.imageAltText}
           />
           <CustomImage
-            className={"gallery-image gallery-image-one image-tablet"}
+            className={"gallery-image image-tablet"}
             src={imageData.third.tablet}
             altText={imageData.third.imageAltText}
           />
           <CustomImage
-            className={"gallery-image gallery-image-one image-desktop"}
+            className={"gallery-image image-desktop"}
             src={imageData.third.desktop}
             altText={imageData.third.imageAltText}
           />
