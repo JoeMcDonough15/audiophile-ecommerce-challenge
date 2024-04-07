d110f590d0d6af3c0b2ff5eb9c4f647da1449c46
Joe McDonough <joemcdonoughmusic@gmail.com>
2024-04-06 21:33:40 -0400

    Add No items in cart text to CartModal if there is nothing in the cart

--- src/components/CartModal/CartModal.tsx
+++ src/components/CartModal/CartModal.tsx
@@ -32,35 +32,43 @@ const CartModal = ({
 
   return (
     <section id="cart-modal" className={cartModalClasses}>
-      <div className="row cart-top-row">
-        <h6 className="black-text">Cart ({numItemsInCart})</h6>
-        <ButtonRemoveAll
-          onClick={() => {
-            removeAllItemsFromCart();
-          }}
-        />
-      </div>
-      <div className="items-in-cart col">
-        {itemsInCart.map((itemInCart: ItemToPurchase) => {
-          return (
-            <ItemRow
-              withSpecifyQuantity
-              key={itemInCart.product.id}
-              itemData={itemInCart}
+      {numItemsInCart === 0 ? (
+        <h6>
+          <em>No items in cart</em>
+        </h6>
+      ) : (
+        <>
+          <div className="row cart-top-row">
+            <h6 className="black-text">Cart ({numItemsInCart})</h6>
+            <ButtonRemoveAll
+              onClick={() => {
+                removeAllItemsFromCart();
+              }}
             />
-          );
-        })}
-      </div>
-      <FeeInformation
-        feeName="Total"
-        amountAsString={fixPrice(calculateSubtotal())}
-      />
-      <ButtonLink
-        className="button-checkout button-dark-orange"
-        buttonText="checkout"
-        buttonDestination={`${ROUTE_PATHS.CHECKOUT}`}
-        onClick={handleCartClick}
-      />
+          </div>
+          <div className="items-in-cart col">
+            {itemsInCart.map((itemInCart: ItemToPurchase) => {
+              return (
+                <ItemRow
+                  withSpecifyQuantity
+                  key={itemInCart.product.id}
+                  itemData={itemInCart}
+                />
+              );
+            })}
+          </div>
+          <FeeInformation
+            feeName="Total"
+            amountAsString={fixPrice(calculateSubtotal())}
+          />
+          <ButtonLink
+            className="button-checkout button-dark-orange"
+            buttonText="checkout"
+            buttonDestination={`${ROUTE_PATHS.CHECKOUT}`}
+            onClick={handleCartClick}
+          />
+        </>
+      )}
     </section>
   );
 };
